5f914ac159b4b84cc25f07c0b67f412e2d17f0f6
Abdullah Mohsin Naqvi <abdullahmohsin21007@gmail.com>
2025-01-16 13:40:19 +0500

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
     'https://localhost:3200', 
     'https://localhost:5173', 
-    'www.fatimanaqvi.com',
-    'https://cattle-tender-mosquito.ngrok-free.app'
 ];
 
 module.exports = allowedOrigins;
