6580bc4d410b388b0f056cf98ddaacf04053e702
mochi_21 <abdullahmohsin21007@gmail.com>
2025-06-11 23:53:16 +0500

    removed manual chunks as it was providong minimal imporovement with less stability and errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,38 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules/chart.js")) {
-            return "admin-dashboard";
-          }
-
-          if (id.includes("src/pages/Dashboards/AdminDashboard")) {
-            return "admin-dashboard";
-          }
-
-          if (id.includes("src/pages/Dashboards/UserDashboard")) {
-            return "user-dashboard";
-          }
-
-          if (
-            id.includes("src/pages/General") ||
-            id.includes("src/components/Header") ||
-            id.includes("src/components/Footer") ||
-            id.includes("src/pages/404.jsx") ||
-            id.includes("src/pages/LoadingPage.jsx")
-          ) {
-            return "common";
-          }
-
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
+  build: {},
 });
