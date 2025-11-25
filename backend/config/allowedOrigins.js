f9e4d596b3c77e2dbfea31194e74026d08694456
mochi_21 <abdullahmohsin21007@gmail.com>
2025-11-25 01:50:35 -0800

    chore: removed dev configs

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,8 +3,6 @@ const allowedOrigins = [
   "http://www.fatimanaqvi.com",
   "https://fatimanaqvi.com",
   "http://fatimanaqvi.com",
-  "http://localhost:5173",
-  "https://cattle-tender-mosquito.ngrok-free.app",
 ];
 
 module.exports = allowedOrigins;
